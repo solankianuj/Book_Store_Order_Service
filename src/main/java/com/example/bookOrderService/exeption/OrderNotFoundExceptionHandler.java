8a57737fddb9b69fab9c9de644da3b9fea7439ee
solanki <anujaj2222.gmail.com>
2022-09-26 22:50:57 +0530

    Book Order Service

--- src/main/java/com/example/bookOrderService/exeption/OrderNotFoundExceptionHandler.java
+++ src/main/java/com/example/bookOrderService/exeption/OrderNotFoundExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.bookOrderService.exeption;
 
+import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.example.bookOrderService.util.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,5 +31,12 @@ public class OrderNotFoundExceptionHandler {
         response.setStatusMsg(Message.toString());
         response.setStatusCode(400);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(SignatureVerificationException.class)
+    public ResponseEntity<Response> handleException(SignatureVerificationException ad) {
+        Response response=new Response();
+        response.setStatusCode(400);
+        response.setStatusMsg("Invalid Token !");
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
